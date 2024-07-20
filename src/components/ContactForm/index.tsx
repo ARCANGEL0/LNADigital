ffc5238e820be8ff2a523fccd947ec9dc31634f9
Henry <henry.arcangello@proton.me>
2024-07-20 12:58:48 +0000

    update src/components/ContactForm/index.tsx

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -3,42 +3,27 @@ import { withTranslation } from "react-i18next";
 import { Slide, Zoom } from "react-awesome-reveal";
 import { ContactProps, ValidationTypeProps } from "./types";
 import { useForm } from "../../common/utils/useForm";
-import validate from "../../common/utils/validationRules";
 import { Button } from "../../common/Button";
 import Block from "../Block";
 import Input from "../../common/Input";
 import TextArea from "../../common/TextArea";
 import { notification } from "antd";
-
 import { ContactContainer, FormGroup, Span, ButtonContainer } from "./styles";
 
-
-const Contact = ({ title, content, id, t, carregar }: ContactProps, ) => {
-
- 
-   const openNotificationWithIcon = (tipo : any) => {
-
-    if(tipo == 'success') {
-
+const Contact = ({ title, content, id, t, carregar }: ContactProps) => {
+  const openNotificationWithIcon = (tipo: 'success' | 'error') => {
+    if (tipo === 'success') {
       notification["success"]({
-      message: t("Success"),
-      description: t("Message sent!"),
-    });
-
-    }
-
-    else if (tipo == 'error' ) {
+        message: t("Success"),
+        description: t("Message sent!"),
+      });
+    } else if (tipo === 'error') {
       notification["error"]({
-      message: t("Error"),
-      description: t("Message not sent!"),
-    });
-
+        message: t("Error"),
+        description: t("Message not sent!"),
+      });
     }
-  
   };
-
-
-
 
   const { values, errors, handleChange, handleSubmit } = useForm(
     carregar, openNotificationWithIcon
@@ -67,20 +52,19 @@ const Contact = ({ title, content, id, t, carregar }: ContactProps, ) => {
               <Col span={24}>
                 <Input
                   type="text"
-                  name="Name"
+                  name="name"
                   placeholder="Your Name"
-                  value={values.name || ""}
+                  value={values.name}
                   onChange={handleChange}
                 />
                 <ValidationType type="name" />
               </Col>
-              
               <Col span={24}>
                 <Input
                   type="text"
-                  name="Email"
+                  name="email"
                   placeholder="Your Email"
-                  value={values.email || ""}
+                  value={values.email}
                   onChange={handleChange}
                 />
                 <ValidationType type="email" />
@@ -88,8 +72,8 @@ const Contact = ({ title, content, id, t, carregar }: ContactProps, ) => {
               <Col span={24}>
                 <TextArea
                   placeholder="Your Message"
-                  value={values.message || ""}
-                  name="Message"
+                  value={values.message}
+                  name="message"
                   onChange={handleChange}
                 />
                 <ValidationType type="message" />
