fb40acf83cef4ab1a960d1e4ef887572e0de62b0
Henry <henry.arcangello@proton.me>
2024-09-01 20:54:51 +0100

    update src/pages/Home/index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -65,20 +65,7 @@ else {
       />
 
       <WhatsIco />
-     
-    <IntroBlock
-        type="right"
-        title={IntroContent.title}
-        content={IntroContent.text}
-        button={IntroContent.button}
-        icon="bannerintro.gif"
-        id="intro"
-                imgId="introgif" 
-                width=""
-                height=""
-        rowClass="introRow"
-
-      />
+   
     <IntroBlock
         type="right"
         title={IntroContent.title}
