b625b4945314d8d37081b73df5c3de4aa0025035
Henry <henry.arcangello@proton.me>
2024-09-01 20:45:52 +0100

    update src/components/WhatsIco/index.tsx

--- src/components/WhatsIco/index.tsx
+++ src/components/WhatsIco/index.tsx
@@ -11,14 +11,14 @@ const Block = ({ title, content, t }: Props) => {
   return (
     
     <a
-        href="https://wa.me/1234567890" // Substitua pelo seu número de WhatsApp
+        href="https://wa.me/351927252945" 
       className="whatsapp-button"
       target="_blank"
       rel="noopener noreferrer">
       
       
       <img
-        src="whatsapp-icon.png" // Substitua pelo caminho do ícone do WhatsApp
+        src="../../../public/img/Icons/Zap.png" // Substitua pelo caminho do ícone do WhatsApp
         alt="WhatsApp"
         width="50" // Ajuste o tamanho conforme necessário
         height="50"
